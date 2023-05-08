cfbb9539c367a3f7c7a5859813e597afc0ca1e54
Lucie <luciedumas24430@gmail.com>
2023-05-08 19:27:33 +0200

    rajout du clavier + test pour faire disparaitre la div de menu et apparaitre la div de jeu au clic d'un bouton

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,17 @@
 
 $(document).ready(function() {
 
+    /*Cacher les div de jeu*/
+    document.getElementById("game-board").style.display = none;
+
     /*Si l'utilisateur choisit 6 lettres*/
     button1 = document.getElementById("6");
 
     button1.addEventListener("click", () => {
+
+        /*Cacher la div de menu et afficher les divs de jeu*/
+        document.getElementById("homeContainer").style.display = none;
+
         /*Appeler le dictionnaire, tous les mots à 6 lettres et en choisir un aléatoirement */
     })
     
@@ -13,11 +20,17 @@ $(document).ready(function() {
     button2 = document.getElementById("7");
 
     button2.addEventListener("click", () => {
+
+        /*Cacher la div de menu et afficher les divs de jeu*/
+        document.getElementById("homeContainer").style.display = none;
     })
 
     /*Si l'utilisateur choisit 8 lettres*/
     button3 = document.getElementById("8");
 
     button3.addEventListener("click", () => {
+
+        /*Cacher la div de menu et afficher les divs de jeu*/
+        document.getElementById("homeContainer").style.display = none;
     })
 });
