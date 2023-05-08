291b6ac0e478ea253f1bf6461abf5a1d774cc058
Lucie24 <82020970+Lucie24@users.noreply.github.com>
2023-05-08 17:00:43 +0200

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,7 @@
+Répartition des tâches :
+
+	- Choisir un mot
+	- Dessiner les cases 
+	- Soumettre une proposition
+	- Vérification de la soumission 
+	- Success et nombres d'essais
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+/*On attend que le DOM soit totalement chargé*/
+
+$(document).ready(function() {
+
+    /*Si l'utilisateur choisit 6 lettres*/
+    button1 = document.getElementById("6");
+
+    button1.addEventListener("click", () => {
+        /*Appeler le dictionnaire, tous les mots à 6 lettres et en choisir un aléatoirement */
+    })
+    
+    /*Si l'utilisateur choisit 7 lettres*/
+    button2 = document.getElementById("7");
+
+    button2.addEventListener("click", () => {
+    })
+
+    /*Si l'utilisateur choisit 8 lettres*/
+    button3 = document.getElementById("8");
+
+    button3.addEventListener("click", () => {
+    })
+});
